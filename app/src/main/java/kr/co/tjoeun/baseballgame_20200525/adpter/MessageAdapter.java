4763fae1736b8036806b15e2659f73aa5c32ab0d
kimhaneui <kimhaneui2@naver.com>
2020-05-25 11:02:29 +0900

    상황에 맞게 대사를 출력  - 말한 사람에 따라 setText의 주체 다르게

--- app/src/main/java/kr/co/tjoeun/baseballgame_20200525/adpter/MessageAdapter.java
+++ app/src/main/java/kr/co/tjoeun/baseballgame_20200525/adpter/MessageAdapter.java
@@ -6,9 +6,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 import java.util.Objects;
@@ -42,6 +45,8 @@ public class MessageAdapter extends ArrayAdapter<Message> {
 //        화면의 컴포넌트들을 가져오는 코드들
         FrameLayout myMessageLayout = row.findViewById(R.id.myMessageLayout);
         FrameLayout cpuMessageLayout = row.findViewById(R.id.cpuMessageLayout);
+        TextView myMessage = row.findViewById(R.id.myMessage);
+        TextView cpuMessage = row.findViewById(R.id.cpuMessage);
 
 //        이번에 보여줘야할 채팅 메세지
         Message data = mList.get(position);
@@ -50,11 +55,15 @@ public class MessageAdapter extends ArrayAdapter<Message> {
 
             myMessageLayout.setVisibility(View.VISIBLE);
             cpuMessageLayout.setVisibility(View.GONE);
+
+            myMessage.setText(data.getContent());
         }
         else{
 
             myMessageLayout.setVisibility(View.GONE);
             cpuMessageLayout.setVisibility(View.VISIBLE);
+
+            cpuMessage.setText(data.getContent());
         }
         return  row;
     }
