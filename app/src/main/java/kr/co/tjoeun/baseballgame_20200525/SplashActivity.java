883686c48a0f73d19e285720b3404e0bfa150ded
kimhaneui <kimhaneui2@naver.com>
2020-05-25 14:39:13 +0900

    스플래쉬화면에서 2초뒤에 메인으로 전환

--- app/src/main/java/kr/co/tjoeun/baseballgame_20200525/SplashActivity.java
+++ app/src/main/java/kr/co/tjoeun/baseballgame_20200525/SplashActivity.java
@@ -2,13 +2,35 @@ package kr.co.tjoeun.baseballgame_20200525;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent myintent = new Intent(mContext,MainActivity.class);
+                startActivity(myintent);
+                finish();
+            }
+        },2000);
+
     }
 }
